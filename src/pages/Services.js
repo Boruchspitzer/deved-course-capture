8beeeabd18d1d1c6d381c3237da3e14b6206c6e3
boruch spitzer <coderoney@gmail.com>
2021-10-15 07:06:47 -0400

    .11 service page styling

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,11 +1,62 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
+//images
+import athlete from '../img/athlete-small.png'
+import goodtimes from '../img/goodtimes-danilevich-5847900.jpg'
+import theracer from '../img/theracer-pexels.jpg'
 
 const Services = () => {
     return (
-        <div>
-            <h2>Services</h2>
-        </div>
+        <Work>
+            <Movie>
+                <h2>The athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="the athlete" />
+                </Link>
+
+            </Movie>
+            <Movie>
+                <h2>Good times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="the good times" />
+                </Link>
+
+            </Movie>
+            <Movie>
+                <h2>The racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="the racer" />
+                </Link>
+
+            </Movie>
+        </Work>
     )
 }
+const Work = styled.div`
+color: white;
+min-height: 100vh;
+overflow: hidden;
+padding: 5rem 10rem;
+h2{
+    padding: 2rem 0rem;
+
+}
+.line{
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+}
+`;
+const Movie = styled.div`
+img{
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+}
+`;
 
 export default Services;
